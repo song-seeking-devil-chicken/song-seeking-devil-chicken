b0a5198302fbdf879b55aaf0476d44a3eb7db0e2
Alex Adams <alexadams@Alexs-MacBook.local>
2022-04-13 20:18:22 -0400

    add response to /api/checkAuth

--- client/authCheck.js
+++ client/authCheck.js
@@ -2,6 +2,7 @@ const authStatus = { authenticated: false };
 
 authStatus.getAuthStatus = function getAuthStatus() {
   fetch('/api/checkAuth').then((res) => res.json()).then((res) => {
+    console.log(res.authenticated);
     if (res.authenticated === true) authStatus.authenticated = true;
     return authStatus.authenticated;
   }).catch(() => authStatus.authenticated);
--- server/routes/checkAuthRoute.js
+++ server/routes/checkAuthRoute.js
@@ -3,8 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log(req.cookies);
-  res.sendStatus(200);
+  res.status(200).json({ authenticated: true });
 });
 
 module.exports = router;
