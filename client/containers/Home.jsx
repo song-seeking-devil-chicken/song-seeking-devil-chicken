e99089fe560cf7a16fc4d28a3ff2a873d048c1c0
Anthony Valdez <avaldez520@gmail.com>
2022-04-14 19:34:20 -0400

    changes to homepage and added favicon

--- client/containers/Home.jsx
+++ client/containers/Home.jsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="home">
       <h1>Song Seeking Devil Chicken</h1>
+      <img src="../img/DevilChickenWithHeadphones.jpg" style={{borderRadius: '100%', border: '10px solid #0e8a3d', height: '60%', width: '60%'}}></img>
       <h3>By: Alex, Anthony, Clare, and Evelin</h3>
     </div>
   );
