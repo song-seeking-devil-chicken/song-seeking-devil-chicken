24d592abace3c877fd58d0a08cc228a59d5ed43f
Alex Adams <alexadams@Alexs-MacBook.local>
2022-04-14 18:25:07 -0400

    material-ui icons

--- client/components/SpotifyPlayer.jsx
+++ client/components/SpotifyPlayer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import SkipNextRoundedIcon from '@mui/icons-material/SkipNextRounded';
+import SkipPreviousRoundedIcon from '@mui/icons-material/SkipPreviousRounded';
 
 export default function Player() {
   return (
     <div className="spotifyPlayer">
-      <button type="button" id="back">B</button>
-      <button type="button" id="play">P</button>
-      <button type="button" id="next">N</button>
+      <button type="button">
+        <SkipPreviousRoundedIcon />
+      </button>
+      {/* <button type="button" id="play">P</button> */}
+      <button type="button">
+        <PlayArrowRoundedIcon />
+      </button>
+      {/* <button type="button" id="next">N</button> */}
+      <button type="button">
+        <SkipNextRoundedIcon />
+      </button>
     </div>
   );
 }
